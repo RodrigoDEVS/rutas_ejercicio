ff1ef2f194503c34bd1a88ece534012990add687
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-05-04 10:24:43 -0500

    agregada la función de Link dentro de un botón

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import RegisterForm from '../components/RegisterForm'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
 const Register = () => {
-  return (
-    <div>
-        <h1>Registro de Usuario</h1>
-        <RegisterForm/>
-        <Link to={'/'} className='btn-primary'>Volver al Inicio</Link>
-    </div>
-  )
+    return (
+        <div>
+            <div className='container'>
+                <h1>Registro de Usuario</h1>
+                <RegisterForm />
+            </div>
+            <div className='mt-2'>
+                <Button variant="secondary"><Link to={'/'} className='btn' style={{color: 'white'}} >Volver al Inicio</Link></Button>
+            </div>
+        </div>
+    )
 }
 
 export default Register
